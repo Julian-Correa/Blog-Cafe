f6a3a0b48d44fdd165d66e5bffcf1dc9f195b215
dessinger <juliancorrea1403@gmail.com>
2022-10-26 05:41:51 -0300

    seleccionando y agregando eventos al boton de formulario

--- scrips.js
+++ scrips.js
@@ -1,6 +1,8 @@
 //seleccionar elemento
 
 const BotonEnviar = document.querySelector('.boton--primario');
-BotonEnviar.addEventListener('enviar',function(){
-    console.log('enviando formulario')
-})
+BotonEnviar.addEventListener('click', function(evento) {
+    console.log(evento)
+   evento.preventDefault();
+    console.log('enviando formulario');
+});
