7d3b446dfa344ce97454ae2f4fcbe6ed146932f5
dessinger <juliancorrea1403@gmail.com>
2022-10-26 14:53:07 -0300

    perfecccionando el formulario

--- scrips.js
+++ scrips.js
@@ -1,4 +1,4 @@
-//seleccionar elemento
+//seleccionar elemento y evento del click
 
 const BotonEnviar = document.querySelector('.boton--primario');
 BotonEnviar.addEventListener('click', function(evento) {
@@ -6,3 +6,30 @@ BotonEnviar.addEventListener('click', function(evento) {
    evento.preventDefault();
     console.log('enviando formulario');
 });
+
+//eventos del los input y del texarea
+
+const datos = {
+    nombre :'',
+    email :'',
+    mensaje : ''
+}
+
+const nommbre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+nombre.addEventListener('input',leerTexto);
+email.addEventListener('input',leerTexto);
+mensaje.addEventListener('input',leerTexto);
+
+function leerTexto(evento){
+    // console.log(e.target.value);
+    datos[evento.target.id] = evento.target.value;
+
+    console.log(evento.target);
+
+    console.log(datos);
+
+
+}
