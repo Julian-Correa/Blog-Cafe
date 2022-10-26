472dd4287d8ad62120824e798a5a8d052ef023bb
dessinger <juliancorrea1403@gmail.com>
2022-10-26 20:31:30 -0300

    agregando scrip para validar el formulario cuando no este completo

--- scrips.js
+++ scrips.js
@@ -1,11 +1,17 @@
 //seleccionar elemento y evento del click
 
-const BotonEnviar = document.querySelector('.boton--primario');
-BotonEnviar.addEventListener('click', function(evento) {
-    console.log(evento)
-   evento.preventDefault();
-    console.log('enviando formulario');
-});
+// const BotonEnviar = document.querySelector('.boton--primario');
+// BotonEnviar.addEventListener('click', function(evento) {
+//     console.log(evento)
+//    evento.preventDefault();
+//     console.log('enviando formulario');
+// });
+
+
+
+
+
+
 
 //eventos del los input y del texarea
 
@@ -18,10 +24,55 @@ const datos = {
 const nommbre = document.querySelector('#nombre');
 const email = document.querySelector('#email');
 const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
 
 nombre.addEventListener('input',leerTexto);
 email.addEventListener('input',leerTexto);
 mensaje.addEventListener('input',leerTexto);
+
+
+// evento del sumbit
+
+
+formulario.addEventListener('submit',function(evento) {
+    evento.preventDefault();
+    
+
+//validando formulario 
+
+    
+ const { nombre, email, mensaje  } = datos;
+
+console.log(nombre);
+console.log(email);
+console.log(mensaje);
+
+if(nombre ==='' || email===''|| mensaje==='' ) { 
+
+    mostrarError('todos los campos son obligatorios');
+
+    return;  //corta con la funcion si se cumple con la condicion
+};
+
+
+console.log('enviando Formulario');
+});
+
+function mostrarError(mensaje) { 
+
+    const error = document.createElement('P');
+    error.textContent= mensaje;
+    error.classList.add('error');
+
+    formulario.appendChild(error);
+
+    //desaparecer despues de 3 segundos 
+    setTimeout(() =>{
+        error.remove()
+    }, 3000);
+
+ };
+
 
 function leerTexto(evento){
     // console.log(e.target.value);
@@ -33,3 +84,4 @@ function leerTexto(evento){
 
 
 }
+
